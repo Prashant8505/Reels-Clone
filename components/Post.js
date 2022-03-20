ad68e70f2c2783f8e26f3342a2d1e6ecce10ce1d
Prashant8505 <Prashantbaghel8505@gmail.com>
2022-03-20 14:37:26 +0530

    some bug fixes

--- components/Post.js
+++ components/Post.js
@@ -4,6 +4,9 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import { AuthContext } from '../context/auth';
 import { arrayRemove, arrayUnion, doc, updateDoc } from 'firebase/firestore';
 import { db } from '../firebase';
+import { PlayArrow } from '@mui/icons-material';
+import { Pause } from '@mui/icons-material';
+import { VolumeOff } from '@mui/icons-material';
 
 function Post({ postData, userData }) {
 
@@ -12,17 +15,54 @@ function Post({ postData, userData }) {
 
     const { user } = useContext(AuthContext)
     const [like, setLike] = useState(false)
-    const [isVideoPlaying, setIsVideoPlaying] = useState(false)
+    const [isVideoPlaying, setIsVideoPlaying] = useState(true)
+    const [mute, setMute] = useState(false)
     const videoRef = useRef(null)
 
-    const onVideoPress = () => {
-        if (isVideoPlaying) {
-            videoRef.current.pause()
-            setIsVideoPlaying(false)
+    useEffect(() => {
+        let options = {
+            rootMargin: "0px",
+            threshold: [0.25, 0.75]
+        };
+
+        let handlePlay = (entries, observer) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+
+                    videoRef.current.play();
+                } else {
+                    setMute(false)
+                    videoRef.current.pause();
+                }
+            });
+        };
+
+        let observer = new IntersectionObserver(handlePlay, options);
+
+        observer.observe(videoRef.current);
+    });
+    // const onVideoPress = () => {
+    //     if (isVideoPlaying) {
+    //         setIsVideoPlaying(false)
+    //     } else {
+    //         setIsVideoPlaying(true)
+    //     }
+    // }
+
+    // const handleScroll = (e) => {
+    //     if (isVideoPlaying) {
+    //         videoRef.current.pause()
+    //         setIsVideoPlaying(false)
+    //     }
+    // }
+
+    const handleMute = () => {
+        if (!mute) {
+            setMute(true)
         } else {
-            videoRef.current.play()
-            setIsVideoPlaying(true)
+            setMute(false)
         }
+
     }
 
     useEffect(() => {
@@ -49,17 +89,24 @@ function Post({ postData, userData }) {
     return (
         <div>
             <div className="post_container">
+
                 <video src={postData.postUrl} style={{ objectFit: 'fill' }}
                     ref={videoRef}
-                    onClick={onVideoPress}
+                    onClick={handleMute}
+                    muted={mute}
                 />
-                <div className="video_info">v
+                <div className="video_info">
+
                     <div className='avatar'>
                         <Avatar alt="Remy Sharp" src={postData.profileUrl}
                             sx={{ margin: '0.5rem' }} />
                         <p style={{ color: 'white', fontWeight: 'bold' }}> {postData.profileName}</p>
                     </div>
+                    <div>
+                        {mute && <VolumeOff className='mute' />}
+                    </div>
                     <div className='post_like' >
+
                         <FavoriteIcon fontSize='large' style={like ? { color: 'red' } : { color: 'white' }}
                             onClick={() => { handleLike() }} />
                         {postData.likes.length > 0 && postData.likes.length}
